80da92eed5db5f25f370725bff588f0019ccaf5e
Difermon <didac.fernandez9@gmail.com>
2026-02-25 06:18:28 +0100

    Create arduino project

---
+++ Robot firmware/diff_drive_controller/diff_drive_controller.ino
@@ -0,0 +1,16 @@
+#define ENCDA 32
+#define ENCDB 33
+#define ENCIA 25
+#define ENCIB 26
+
+// Definición de pines (ejemplo)
+const int AIN1 = 13;
+const int AIN2 = 14;
+const int PWMA = 12;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+}
